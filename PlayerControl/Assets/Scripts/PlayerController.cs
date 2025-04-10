a3a6d242efc9e62ad504f76a1ffdccaea1d926de
Zachary Vasquez <vasquezz@roverkids.org>
2025-04-10 14:11:16 -0400

    done with 1.4 part 4

--- PlayerControl/Assets/Scripts/PlayerController.cs
+++ PlayerControl/Assets/Scripts/PlayerController.cs
@@ -3,16 +3,23 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     private float speed = 20;
+    public float turnSpeed;
+    public float horizontalInput;
+    public float forwardInput;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         
     }
     
-    // Update is called once per frame
+    // Update is cSalled once per frame
     void Update()
     {
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
         // We'll move the vehicle foward
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
+
     }
 }
